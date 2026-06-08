714f121e21b09872b66ea331740c505ab5f6a75f
khibridev <yassine14kh@gmail.com>
2026-06-08 11:51:16 +0100

    suppression ancien projet

--- dashboard/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin/', views.dashboard_admin, name='dashboard_admin'),
-    path('client/', views.dashboard_client, name='dashboard_client'),
-]

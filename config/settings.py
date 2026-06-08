c75942fdc80bc0af1b6f8eb41ca320dd29fcb359
khibridev <yassine14kh@gmail.com>
2026-06-08 11:42:54 +0100

    fix csrf

--- config/settings.py
+++ config/settings.py
@@ -126,4 +126,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://gestionpiecess.up.railway.app']

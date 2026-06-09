16ed11f634ebd95d0d9b1ec7ea92da077129327d
khibridev <yassine14kh@gmail.com>
2026-06-09 08:58:01 +0100

    fix duplicate pieces app

--- gestion_pieces/settings.py
+++ gestion_pieces/settings.py
@@ -16,7 +16,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pieces',
     'cloudinary',
     'cloudinary_storage',
     'pieces',
